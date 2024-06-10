dab941e25c2473a711fd1745e2f9828bbaa81061
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-10 14:38:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppProjects from './components/AppProjects.vue';
+  
+
+export default {
+  components: {
+    AppProjects,
+
+  },
+
+  data() {
+    return {
+      store
+    };
+  },
+  
+  methods: {
+   
+  },
+  mounted() {
+   
+  }
+}
+</script>
+
+<template>
+  <AppProjects>BOOOOOOOOOOOOOOOOOOBS</AppProjects>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppProjects.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'AppProjects',
+
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            
+            }
+        }
+	
+
+    
+</script>
+
+<template>
+<div>BOOOOOOOOOOOOOOOOOOBS</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
