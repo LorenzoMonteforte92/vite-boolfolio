68e5cc61a71c76685fe6671aa73152a1e91ea0bb
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-11 17:57:04 +0200

    feat: add single post api

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,33 +1,47 @@
 <script>
+
+import axios from 'axios';
+
     export default {
         name: 'SingleProject',
 
         data(){
             return{
-                
+                project: null,
             }
         },
         methods: {
             
+            getSinglePostFromApi(){
+                axios.get(`http://127.0.0.1:8000/api/projects/${this.$route.params.slug}`)
+                .then((response) => {
+                this.project = response.data.project;
+                console.log(this.project)
+      })
             }
-        }
+         },
+
+         mounted() {
+        this.getSinglePostFromApi();
+    }
+    }
 	
 
     
 </script>
 
 <template>
-     <div class="container">
+     <div v-if="project" class="container">
         <div class="row mb-3">
-            <h1>Project: [name]</h1>
+            <h1>Project: {{ project.name }}</h1>
         </div>
         <div class="row">
             <div class="col">
                 <div class="card" style="width: 18rem;" >
-                <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <img :src="`http://127.0.0.1:8000/storage/${project.image}`" class="card-img-top" alt="...">
                     <div class="card-body" >
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">{{ project.name }}</h5>
+                        <p class="card-text">{{ project.summary }}</p>
                     </div>
                 </div>
             </div>
