0835ab52883b12391ce63b19c9a9adbf11ac69f8
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-11 17:02:36 +0200

    feat: add button to cards

--- src/components/ProjectsCard.vue
+++ src/components/ProjectsCard.vue
@@ -21,9 +21,12 @@
 
 <template>
     <div class="card" style="width: calc(100% / 4);">
-        <div class="card-body">
+        <div class="card-body d-flex flex-column justify-content-between">
             <h5 class="card-title">{{ projectInfo.name }}</h5>
             <p v-if="projectInfo.summary " class="card-text">{{ projectInfo.summary }}</p>
+            <div >
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
     </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppAbout from './pages/AppAbout.vue';
 import AppProjects from './pages/AppProjects.vue';
+import SingleProject from './pages/SingleProject.vue';
+
 
 const router = createRouter({	
     history: createWebHistory(),
@@ -21,6 +23,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: AppProjects
+        },
+        {
+            path: '/projects/:slug',
+            name: 'Single-project',
+            component: SingleProject
         },
     ]
 });
