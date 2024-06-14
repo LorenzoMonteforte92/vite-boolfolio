977272b8dbcca5d5bb731d3e583b065189273238
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-14 14:28:12 +0200

    feat: add pagination

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -15,22 +15,33 @@ import { store } from '../store.js';
             return{
                 projects: [],
                 store,
+                currentPage: 1,
+                prevPage: null,
+                nextPage: null,
             }
         },
         
         methods: {
             
-            getProjects() {
-                axios.get(`${this.store.apiBaseURL}/api/projects`)
+            getProjects(pageNumber) {
+                axios.get(`${this.store.apiBaseURL}/api/projects`,{
+                    params: {
+                        page: pageNumber,
+                    }
+                })
                 .then((response) => {
                 this.projects = response.data.results.data;
-                console.log(this.projects)
+                this.currentPage = response.data.results.current_page;
+                this.prevPage = response.data.results.prev_page_url;
+                this.nextPage = response.data.results.next_page_url;  
+                console.log(this.prevPage);
+                console.log(this.nextPage);
         })
     }
 },
 
     mounted(){
-        this.getProjects()
+        this.getProjects(this.currentPage)
         
     }
  }
@@ -45,21 +56,31 @@ import { store } from '../store.js';
         <div class="row">
             <nav aria-label="Page navigation example">
                 <ul class="pagination justify-content-around">
-                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    <li class="page-item" :class="{disabled: !this.prevPage}"  v-if="!this.prevPage" ><a class="page-link">First Page</a></li>
+                    <li class="page-item" v-if="this.prevPage" ><a class="page-link" @click="getProjects(currentPage - 1)" >Previous</a></li>
+                    <li class="page-item pt-2"><span class="fw-bold text-primary" >{{currentPage}}</span></li>
+                    <li class="page-item" v-if="this.nextPage" ><a class="page-link" @click="getProjects(currentPage + 1)" >Next</a></li>
+                    <li class="page-item" :class="{disabled: !this.nextPage}"  v-if="!this.nextPage" ><a class="page-link">Last Page</a></li>
                 </ul>
             </nav>
             <div class="col d-flex flex-wrap gap-3 justify-content-center">
                 <ProjectCard v-for="singleProject in projects" :projectInfo="singleProject" ></ProjectCard>
             </div>
             <ul class="pagination justify-content-around mt-3">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <li class="page-item" :class="{disabled: !this.prevPage}"  v-if="!this.prevPage" ><a class="page-link">First Page</a></li>
+                <li class="page-item" v-if="this.prevPage" ><a class="page-link" @click="getProjects(currentPage - 1)" >Previous</a></li>
+                <li class="page-item pt-2"><span class="fw-bold text-primary"  >{{currentPage}}</span></li>
+                <li class="page-item" v-if="this.nextPage" ><a class="page-link" @click="getProjects(currentPage + 1)" >Next</a></li>
+                <li class="page-item" :class="{disabled: !this.nextPage}" v-if="!this.nextPage" ><a class="page-link">Last Page</a></li>
             </ul>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-
+    .pagination{
+        .page-item{
+            cursor: pointer;
+        }
+    }
 </style>
