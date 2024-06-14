7875802cf9506260c33a6a253da75f402b4ea1d5
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-14 12:05:01 +0200

    fix: not found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,22 +1,11 @@
 <script>
-    export default {
-        name: 'NotFound',
-        
-
-        data(){
-            return{
-                
-            }
-        },
-        methods: {
-            
-            }
-        }
-	
-
-    
+export default {
+    name: 'NotFound'
+}
 </script>
 
 <template>
-    
+    <div class="container">
+        <h2>Page not found</h2>
+    </div>
 </template>
